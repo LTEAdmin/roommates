04098c9c94a8d692b7e4c3e37608eed021922df5
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-21 22:33:22 -0400

    :sparkles: se continua a trabajando con lso gastos

--- src/controllers/gastosCtrl.js
+++ src/controllers/gastosCtrl.js
@@ -1,3 +1,8 @@
+import {
+    agregarGastosQuery,
+    buscarGastosQuery
+} from "../querys/gastosQuery";
+
 export const agregarGastosCtrl = async (req, res) => {
     try {
         const result = await agregarGastosQuery(req, res);
@@ -8,3 +13,24 @@ export const agregarGastosCtrl = async (req, res) => {
         res.status(500).send("Error: " + error);
     };
 };
+
+export const buscarGastos = async (req, res) => {
+    try {
+        const result = await buscarGastosQuery();
+        res.json(result);
+    }
+    catch (error) { 
+        res.status(500).send("Error: " + error);
+    };
+}
+export const borrarGastos = async (req, res) => {
+    const { id } = req.query;
+    try {
+        const result = await borrarGastosQuery();
+        response = await calcularCuentas();
+        res.status(200).send(result);
+    }
+    catch (error) { 
+        res.status(500).send("Error: " + error);
+    };
+}
--- src/querys/gastosQuery.js
+++ src/querys/gastosQuery.js
@@ -1,8 +1,38 @@
+import fs from "fs";
+
+
 export const agregarGastosQuery = async (req, res) => {
-    try { 
+    try {
         const { name, descripcion, monto } = req.body;
-    const id=uuidv4().slice(4,12);    }
+        const id = uuidv4().slice(4, 12);
+        const gastos = JSON.parse(fs.readFileSync("../data/gastos.Json", "utf8"));
+        gastos.push({ id, name, descripcion, monto });
+        fs.writeFileSync("../data/gastos.Json", JSON.stringify({ gastos }));
+        return (gastos);
+    }
     catch (error) {
         res.status(500).send("Error: " + error);
     };
 };
+
+export const buscarGastosQuery = async () => {
+    try {
+        const gastos = JSON.parse(fs.readFileSync("../data/gastos.Json", "utf8"));
+        return (gastos);
+    }
+    catch (error) {
+        res.status(500).send("Error: " + error);
+    };
+}
+
+export const borrarGastosQuery = async (id) => {
+    try {
+        const gastos = JSON.parse(fs.readFileSync("../data/gastos.Json", "utf8"));
+        fs.writeFileSync("../data/gastos.Json", JSON.stringify({ gastos }));
+        gastos=gastos.filter(gasto => gasto.id !== id);
+        fs.writeFileSync("../data/gastos.Json", JSON.stringify({ gastos }));
+    }
+    catch (error) {
+        res.status(500).send("Error: " + error);
+    };
+}
